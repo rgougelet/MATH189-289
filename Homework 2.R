703695ad8c7c457fabdee9294f571aa2310402da
sidney browne <sidneybrowne@sidneys-MacBook-Pro.local>
2017-01-31 17:30:35 -0800

    added scenario 3 sid

--- Homework 2.R
+++ Homework 2.R
@@ -13,6 +13,9 @@ data[data == 99] <- NA
 ## check to see how the amnt of time spent playing games in the week 
 # prior to the survey compares to the reported frequency of play (daily,
 # weekly, etc)
+## how might the fact that there was an exam the week prior to the survey 
+# affect the estimates of scenario 1, and this comparason
+
 attach(data)
 # var name: time; meaning: number of hours played in the week prior to the survey
 summary(time)
@@ -43,7 +46,7 @@ semesterly.percent <- 1 - length(semesterly0.ind)/length(semesterly.ind) # 4.3%
 
 ## histograms of hours played by reported frequency
 # break points tailored to best display data
-histogram(~data$time|data$freq, breaks = c(0,1,2,3,4,5,6,13,14,15,29,30))
+histogram(~data$time|data$freq, breaks = c(0,.5,1.5,2.5,3.5,4.5,5.5,6,13.5,14.5,15,29.5,30))
 
 hist(daily$time, probability = TRUE, breaks = c(0,1,2,3,4,5,6,13,14))
 rug(daily$time, col = 2, ticksize = .1)
@@ -72,6 +75,15 @@ hist(monthly$time, probability = TRUE, breaks = c(0,.1,.4,.5,1,2),
      xlim = c(0,2))
 hist(semesterly$time, probability = TRUE, breaks = c(0,.05,.95,1.05),
      xlim = c(0,2))
+# scenario 3 --------------------------------------------------------
+## make an interval estimate for the average amount of time spent playing videogames
+# in the week prior to the survey.
+## keep in mind the overall shape of the sample distribution. i.e. do a simulation study
+avg.time <- mean(data$time) # 1.24 hrs
+hist(data$time, probability = TRUE, breaks = c(0,1,2,3,4,5,13,14,29,30))
+lines(density(data$time), col = 2)
+# clearly not a normal distribution, suggests the use of bootstrap
+
 
 
     
