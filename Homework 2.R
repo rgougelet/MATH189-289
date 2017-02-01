5b2ba9d1b32b5cecfd710319ecd828e1c1b1a76f
sidney browne <sidneybrowne@sidneys-MacBook-Pro.local>
2017-01-31 18:39:44 -0800

    scenario 3 more or less done

--- Homework 2.R
+++ Homework 2.R
@@ -13,8 +13,12 @@ data[data == 99] <- NA
 ## check to see how the amnt of time spent playing games in the week 
 # prior to the survey compares to the reported frequency of play (daily,
 # weekly, etc)
-## how might the fact that there was an exam the week prior to the survey 
+
+## Q: how might the fact that there was an exam the week prior to the survey 
 # affect the estimates of scenario 1, and this comparason
+### A: Because there were students in all reported frequencies of play who 
+# played 0 hours the week prior to the survey it is likley that the exam caused 
+# students to play less or not at all
 
 attach(data)
 # var name: time; meaning: number of hours played in the week prior to the survey
@@ -75,14 +79,50 @@ hist(monthly$time, probability = TRUE, breaks = c(0,.1,.4,.5,1,2),
      xlim = c(0,2))
 hist(semesterly$time, probability = TRUE, breaks = c(0,.05,.95,1.05),
      xlim = c(0,2))
+
+
 # scenario 3 --------------------------------------------------------
 ## make an interval estimate for the average amount of time spent playing videogames
 # in the week prior to the survey.
 ## keep in mind the overall shape of the sample distribution. i.e. do a simulation study
 avg.time <- mean(data$time) # 1.24 hrs
+std.dev.time <- sd(data$time) # 3.78
 hist(data$time, probability = TRUE, breaks = c(0,1,2,3,4,5,13,14,29,30))
 lines(density(data$time), col = 2)
+
 # clearly not a normal distribution, suggests the use of bootstrap
+
+# generate a bootstrap population
+boot.pop <- rep(data$time, length.out = 314)
+B = 1000
+boot.sample <- array(dim = c(B,91))
+for(i in 1:B){
+  boot.sample[i, ] <- sample(boot.pop, size = 91, replace = FALSE)
+}
+boot.mean <- apply(X = boot.sample, MARGIN = 1, FUN = mean)
+std.dev.boot.mean <- sd(boot.mean) # .312 use this as the sd of the sample mean
+hist(boot.mean, probability = TRUE, breaks = 20)
+lines(density(boot.mean), col = 2)
+
+# is this normal?
+qqnorm(boot.mean)
+qqline(boot.mean, col = 2)
+shapiro.test(boot.mean) # p-value = 4.949e-06, W = 0.99055 
+# sample mean is a good canidate for normal approx.
+
+# using a normal approx for a 95% CI
+CI95.norm <- avg.time + c(-1,1)*qnorm(.975)*std.dev.boot.mean # .631, 1.855
+
+# using the student t approx for a 95% CI
+# investigate what the df should be, chose sample size (91) - 1
+CI95.t <- avg.time + c(-1,1)*qt(.975, df = 90)*std.dev.boot.mean # .623, 1.863
+
+# using the bootstrap distribution to get a 95% CI
+CI95.boot <- quantile(boot.mean, probs = c(.025, .975)) # .59, 1.77
+
+# all three intervals are similar to each other (because the sample mean 
+# was a good canidate for a normal approx) 
+
 
 
 
