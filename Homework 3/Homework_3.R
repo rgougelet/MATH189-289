56cd6aef10658e6c81d1de6a50ba68dd238de72a
Robert Gougelet <rgougele@gmail.com>
2017-02-16 18:18:49 -0800

    worked on subintervals

--- Homework 3/Homework_3.R
+++ Homework 3/Homework_3.R
@@ -1,6 +1,47 @@
 setwd("C:\\Users\\Rob\\Documents\\MATH189-289\\Homework 3")
 data <- read.table("hcmv.txt", header = TRUE)
+Location <- data$location
+hist(Location, main="Palindromes on DNA sequence", breaks = 50)
+hist(Location, main="Palindromes on DNA sequence", breaks = 100)
+hist(Location, main="Palindromes on DNA sequence", breaks = 200)
+
+
+library(lattice)
+stripplot(Location, main="Occurrence of Palindromes on DNA sequence", pch=16, cex=0.25)
+
+
+# Distances ---------------------------------------------------------------
 Distance <- diff(data$location)
-hist(Distance, main="Distance Between Palindromes", prob=TRUE, breaks = 100)
+hist(Distance, main="Distance Between Palindromes on DNA Sequence", prob=TRUE, breaks = 50)
 lines(density(Distance), col="blue", lwd=2)
-sum(Distance<10)
+
+# Simulate ------------------------------------
+N <- 200000
+n <- 300
+IntLocations <- seq(1, N)
+UniformSimulated <- sample.int(N, size=n) #Uniform
+stripplot(UniformSimulated,main="Occurrence of Palindromes on DNA sequence", pch=16, cex=0.25)
+
+library(lattice)
+ZLocations <- seq(-3, 3, length.out=N)
+NormalSimulated <- sample.int(N, size=n, prob=dnorm(ZLocations))
+stripplot(NormalSimulated,main="Occurrence of Palindromes on DNA sequence", pch=16, cex=0.25)
+
+DoubleLocations <- sample.int(N, size=30)
+Weights <- rep(0.5, N)
+Weights[DoubleLocations] <- Weights[DoubleLocations] + 0.5
+DoubleSimulated <- sample.int(N, size=n, prob=Weights)
+
+
+regionsplit <- function(numOfRegions, refSeq, site){ #number of regions, reference sequence, simulated sequence
+	count.int <- table(cut(site, breaks = seq(1, length(refSeq), length.out=numOfRegions+1), include.lowest=TRUE))
+	count.vector <- as.vector(count.int)
+	count.vector
+	count.tab <- table(count.vector)
+	count.tab
+	return (count.tab)
+}
+
+regionsplit(1000, IntLocations, UniformSimulated)
+regionsplit(10, IntLocations, NormalSimulated)
+regionsplit(11, IntLocations, DoubleSimulated)
